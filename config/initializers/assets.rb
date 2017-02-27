ba295f9aad8bd8acd724449b5ff4cf93d79c2502
Nathan Moore <n@nmoore.net>
2017-02-27 13:22:36 -0700

    add logo, update navg

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( admin/admin.scss )
+Rails.application.config.assets.precompile += %w( admin/theme.scss )
 Rails.application.config.assets.precompile += %w( admin/custom.scss )
