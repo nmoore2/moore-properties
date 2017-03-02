54d99db591b8de30ce68b50aff725e4632803372
Nathan Moore <n@nmoore.net>
2017-03-02 08:53:02 -0700

    replace document ready with turbolinks:load

--- app/assets/javascripts/vendor/preloader.js
+++ app/assets/javascripts/vendor/preloader.js
@@ -1,7 +1,6 @@
 //<![CDATA[
         $(document).on('turbolinks:load', function() {// wait for turbolinks to load page
             $('#preloader').delay(350).fadeOut('slow', function() {
-              console.log($(this));
             });
             // will fade out the white DIV that covers the website.
             $('body').delay(350).css({'overflow':'visible'});
