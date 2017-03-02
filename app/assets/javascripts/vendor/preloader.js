bfe213ec8a9d72752273d5b646838cf614a25e6e
Nathan Moore <n@nmoore.net>
2017-03-02 08:43:50 -0700

    update page loader to wait for turbolinks

--- app/assets/javascripts/vendor/preloader.js
+++ app/assets/javascripts/vendor/preloader.js
@@ -1,7 +1,8 @@
 //<![CDATA[
-        $(document).ready(function() { // makes sure the whole site is loaded
-            $('#preloader').delay(350).fadeOut('slow');
-
+        $(document).on('turbolinks:load', function() {// wait for turbolinks to load page
+            $('#preloader').delay(350).fadeOut('slow', function() {
+              console.log($(this));
+            });
             // will fade out the white DIV that covers the website.
             $('body').delay(350).css({'overflow':'visible'});
         })
